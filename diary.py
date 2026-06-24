beea73df52fa0db377a1dce2c0fde6ba2abc9908
Ella Wallace <ellahopewallace@gmail.com>
2026-06-24 13:29:44 +0100

    Mood logging implemented

--- diary.py
+++ diary.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 def saveDiaryEntry():
     with open("diary.txt", "a") as f:
@@ -28,17 +29,55 @@ def loadDiaryEntries():
         pass
     return diary
 
+def loadMotivationalMessages():
+    messages = {"1":["happy"], "2":["sad"], "3":["frustrated"], "4":["fine"], "5":["stressed"]}
+    try:
+        with open("quotes.txt", "r") as f:
+            lines = f.readlines()
+            for i in range (0, len(lines)-1, 2):
+                mood = lines[i].strip()
+                message = lines[i+1].strip()
+                if mood in messages:
+                    messages[mood].append(message)
+    except FileNotFoundError:
+        pass
+    return messages
+
+def moodDependingMessage(mood):
+    randomNumber = random.randint(1,len(motivationalMessages[mood])-1)
+    randomMessage = motivationalMessages[mood][randomNumber]
+    return randomMessage
+
+def logMood():
+    print ("\nKEY:")
+    print ("1 -> Happy/good.")
+    print ("2 -> Sad/down/disappointed.")
+    print ("3 -> Frustrated/angry/annoyed.")
+    print ("4 -> Okay/fine/neutral.")
+    print ("5 -> Anxious/worried/stressed.")
+    mood = input("How are you feeling today?\nType a number from 1 - 5 according to the key: ")
+    if mood not in motivationalMessages:
+        print("\nPlease only input a number 1-5")
+        return logMood()
+    else:
+        print("\nYou logged a " + motivationalMessages[mood][0] + " mood.\n" + moodDependingMessage(mood)+"\n")
+
 
 diary = loadDiaryEntries()
+motivationalMessages = loadMotivationalMessages()
 
 print("\nWelcome to your diary! You can...")
 print("write a diary entry and read past diary entries by date\n")
-nextInput = input("What would you like to do?\nd -> diary entry\nr -> read diary\nq -> quit\n ")
+nextInput = input("What would you like to do?\nm -> log mood\nd -> diary entry\nr -> read diary\nq -> quit\n ")
 
 while nextInput != "q":
 
+    # mood entry
+    if nextInput == "m":
+        logMood()
+
     # diary entry
-    if nextInput == "d":
+    elif nextInput == "d":
         saveDiaryEntry()
 
     # read past diary entry by date
@@ -53,7 +92,7 @@ while nextInput != "q":
     else:
         print("Please only input d, r, or q")
         
-    nextInput = input("What would you like to do?\nd -> diary entry\nr -> read diary\nq -> quit\n ")
+    nextInput = input("What would you like to do?\nm -> log mood\nd -> diary entry\nr -> read diary\nq -> quit\n ")
 
 print("Hope you had a reflective session! Your diary entries are saved for you when you come back.")
 
